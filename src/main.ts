ae6e52fdfc33354ad650105d1a033af2d67c4bcd
Anthony Gubler <agubler@sitepen.com>
2017-05-13 16:24:28 +0100

    very simple grid

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { ProjectorMixin } from '@dojo/widget-core/mixins/Projector';
-import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { SimpleGrid } from './widgets/SimpleGrid';
 
 const root = document.querySelector('my-app') || undefined;
 
-const Projector = ProjectorMixin(WidgetBase);
+const columns = [
+	{ id: 'id' },
+	{ id: 'name' },
+	{ id: 'address' },
+	{ id: 'birthday' }
+];
+
+const data = [];
+
+for (let i = 0; i < 50; i++) {
+	data.push({ id: i, name: 'Ant', address: '123 High Street', birthday: new Date() });
+}
+
+const Projector = ProjectorMixin(SimpleGrid);
 const projector = new Projector();
+
+projector.setProperties({ data, columns });
 
 projector.append(root);
---
+++ src/widgets/SimpleGrid.ts
@@ -0,0 +1,63 @@
+import { WidgetBase } from '@dojo/widget-core/WidgetBase';
+import { v, w } from '@dojo/widget-core/d';
+import { DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
+
+export interface SimpleGridProperties extends WidgetProperties {
+	data: any[];
+	columns: any[];
+}
+
+export class SimpleGrid extends WidgetBase<SimpleGridProperties> {
+	protected render(): DNode {
+		const { data, columns } = this.properties;
+		return v('div', {
+			styles: {
+				width: '1000px',
+				height: '500px',
+				border: '1px solid #ddd',
+				overflow: 'scroll'
+			}
+		}, data.map((row) => {
+			return w(SimpleGridRow, { data: row, columns });
+		}));
+	}
+}
+
+export interface SimpleGridRowProperties extends WidgetProperties {
+	data: any;
+	columns: any[];
+}
+
+export class SimpleGridRow extends WidgetBase<SimpleGridRowProperties> {
+	protected render(): DNode {
+		const { data, columns } = this.properties;
+
+		return v('div', {
+			styles: {
+				display: 'flex'
+			}
+		}, columns.map((column) => {
+			const item = data[column.id];
+			return w(SimpleGridCell, { item, column });
+		}));
+	}
+}
+
+export interface SimpleGridCellProperties extends WidgetProperties {
+	item: any;
+	column: any;
+}
+
+export class SimpleGridCell extends WidgetBase<SimpleGridCellProperties> {
+	protected render(): DNode {
+		const { item, column } = this.properties;
+
+		return v('div', {
+			styles: {
+				flex: '1',
+				border: '1px solid #ddd',
+				'border-top-style': 'none'
+			}
+		}, [ item.toString() ]);
+	}
+}
