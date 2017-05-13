2542404ba7596b1276e8d4cb13046c3baca986cd
Anthony Gubler <agubler@sitepen.com>
2017-05-13 17:02:50 +0100

    highlight selected row

--- src/widgets/SimpleGrid.ts
+++ src/widgets/SimpleGrid.ts
@@ -43,10 +43,24 @@ export interface SimpleGridBodyProperties extends WidgetProperties {
 
 @theme(css)
 export class SimpleGridBody extends ThemeableMixin(WidgetBase)<SimpleGridBodyProperties> {
+
+	private _selectedRow: string;
+
+	private _selectRow(id: string) {
+		this._selectedRow = id;
+		this.invalidate();
+	}
+
 	protected render(): DNode {
 		const { data, columns } = this.properties;
 		return v('div', { classes: this.classes(css.body) }, data.map((row) => {
-			return w(SimpleGridRow, { key: row.id, data: row, columns });
+			return w(SimpleGridRow, {
+				key: row.id,
+				data: row,
+				columns,
+				onSelectRequest: this._selectRow,
+				selected: this._selectedRow === row.id
+			});
 		}));
 	}
 }
@@ -54,14 +68,24 @@ export class SimpleGridBody extends ThemeableMixin(WidgetBase)<SimpleGridBodyPro
 export interface SimpleGridRowProperties extends WidgetProperties {
 	data: any;
 	columns: any[];
+	onSelectRequest: (id: string) => void;
+	selected: boolean;
 }
 
 @theme(css)
 export class SimpleGridRow extends ThemeableMixin(WidgetBase)<SimpleGridRowProperties> {
-	protected render(): DNode {
-		const { data, columns } = this.properties;
 
-		return v('div', { classes: this.classes(css.row) }, columns.map((column) => {
+	private _onClick(event: MouseEvent) {
+		this.properties.onSelectRequest(this.properties.data.id);
+	}
+
+	protected render(): DNode {
+		const { data, columns, selected } = this.properties;
+
+		return v('div', {
+			classes: this.classes(css.row, selected ? css.selected : null),
+			onclick: this._onClick
+		}, columns.map((column) => {
 			const item = data[column.id];
 			return w(SimpleGridCell, { key: column.id, item, column });
 		}));
